ffb3937caffc27c9501f407e35926d38a3f04984
tommilostny <tommilostny@live.com>
2021-09-28 16:18:03 +0200

    Added test migration for updated user class

--- Fituska/Fituska/Server/Data/Migrations/20210928140309_UserTestMigration.Designer.cs
+++ Fituska/Fituska/Server/Data/Migrations/20210928140309_UserTestMigration.Designer.cs
@@ -4,14 +4,16 @@ using Fituska.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Fituska.Server.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210928140309_UserTestMigration")]
+    partial class UserTestMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
