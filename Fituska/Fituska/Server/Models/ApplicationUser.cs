8cc21a0b5651b153f55b52cebbaf584facbc98d8
tommilostny <tommilostny@live.com>
2021-09-28 15:55:13 +0200

    Updated app user class

--- Fituska/Fituska/Server/Models/ApplicationUser.cs
+++ Fituska/Fituska/Server/Models/ApplicationUser.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Fituska.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string DiscordUserName { get; set; }
+
+        public DateTime RegistrationDate { get; set; }
+
+        public DateTime LastLoginDate { get; set; }
+
+        public string PhotoID { get; set; }
     }
 }
