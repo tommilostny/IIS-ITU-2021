27587036f289291dbfde1d6488f2e52501719a73
rivolacz <m.rivola@seznam.cz>
2021-10-25 23:12:35 +0200

    Added user mappers

---
+++ Fituska/Fituska.Server/Mappers/Users/UserDetailModelMapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using Fituska.Server.Models.DetailModels;
+using Fituska.Server.Models.ListModels;
+using AutoMapper;
+
+namespace Fituska.Server.Mappers;
+public class UserDetailModelMapperProfile : Profile
+{
+    public UserDetailModelMapperProfile()
+    {
+        CreateMap<UserEntity, UserDetailModel>();
+    }
+}
+
--- Fituska/FituskaTests.BL/UserRepositoryTests.cs
+++ Fituska/FituskaTests.BL/UserRepositoryTests.cs
@@ -4,12 +4,13 @@ public class UserRepositoryTests
     private readonly IDbContextFactory dbContextFactory;
     private readonly FituskaDbContext dbContext;
     private readonly UserRepository userRepository;
-
-    public UserRepositoryTests()
+    private readonly IMapper mapper;
+    public UserRepositoryTests(IMapper _mapper)
     {
         dbContextFactory = new InMemoryDbContextFactory(nameof(UserRepositoryTests));
         dbContext = dbContextFactory.Create();
         userRepository = new UserRepository(dbContextFactory);
+        mapper = _mapper;
     }
 
     public async Task InitializeAsync() => await dbContext.Database.EnsureCreatedAsync();
@@ -17,8 +18,41 @@ public class UserRepositoryTests
     public async Task DisposeAsync() => await dbContext.DisposeAsync();
    
     [Fact]
-    public void Test1()
+    public void GetAllUsers()
     {
+        List<UserEntity> users = new()
+        {
+            new UserEntity
+            {
+                FirstName = "Name",
+                LastName = "Surname",
+                AccessFailedCount = 0,
+                DiscordUsername = "DiscordName",
+                Email = "email",
+                EmailConfirmed = true,
+                RegistrationDate = new DateTime(2021, 10, 10, 10, 59, 50),
+            },
+            new UserEntity
+            {
+                FirstName = "Name2",
+                LastName = "Surname2",
+                AccessFailedCount = 4,
+                DiscordUsername = "DiscordName2",
+                Email = "email3",
+                EmailConfirmed = false,
+                RegistrationDate = new DateTime(2020, 10, 10, 10, 59, 50),
+            }
+        };
+        dbContext.Users.AddRange(users);
+        dbContext.SaveChanges();
 
+        using var database = dbContextFactory.Create();
+        var usersFromDb = database.Users.ToList();
+
+        List<UserListModel> listModels = new List<MusicBandListModel>();
+        foreach (MusicBandEntity bandFromDb in bandsFromDb)
+        {
+            listModels.Add(MusicBandMapper.MapEntityToListModel(bandFromDb));
+        }
     }
 }
