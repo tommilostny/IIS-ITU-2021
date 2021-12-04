fcfce3ffdf6efba9c76f64b87e187ca9a3b583db
rivolacz <m.rivola@seznam.cz>
2021-12-04 15:46:53 +0100

    Updated getting user authentication, added vote value neutral

---
+++ Fituska/Fituska.Shared/Static/GetUserInfo.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Fituska.Shared.Static;
+
+public static class GetUserInfo
+{
+    public static async Task<Guid> GetUserId(Task<AuthenticationState> AuthenticationState)
+    {
+        Guid userId;
+        try {
+            userId = new Guid((await AuthenticationState).User.Claims.First(claim => claim.Type == ClaimTypes.NameIdentifier).Value);
+        }
+        catch{
+            userId = Guid.Empty;
+        }
+        return userId;
+    }
+}
